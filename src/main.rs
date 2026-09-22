1fddd0e44579b0496c8afe069ea4076afadea35f
Benj181 <me@benjaminaas.com>
2026-09-22 18:43:50 +0200

    Transition from bevy to crossterm, pure rust implenatation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-mod player;
-use player::PlayerPlugin;
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode},
+    execute, terminal,
+};
+use std::io::stdout;
 
-use bevy::prelude::*;
+fn main() -> std::io::Result<()> {
+    terminal::enable_raw_mode()?;
+    execute!(stdout(), cursor::Hide)?;
 
-fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, PlayerPlugin))
-        .add_systems(Startup, setup)
-        .run();
-}
+    loop {
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    execute!(stdout(), cursor::Show)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
