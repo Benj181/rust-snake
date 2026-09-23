5e9729a868f379bda515b4a2ad799818a81af696
Benj181 <me@benjaminaas.com>
2026-09-23 12:36:35 +0200

    added some structs, starting to implemnt data representations

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ fn main() -> std::io::Result<()> {
     terminal::enable_raw_mode()?;
     execute!(stdout(), cursor::Hide)?;
 
+    let mut snake = Snake::new();
     loop {
-        if event::poll(std::time::Duration::from_millis(100))? {
-            if let Event::Key(key) = event::read()? {
+        if let Ok(true) = event::poll(std::time::Duration::from_millis(100)) {
+            if let Ok(Event::Key(key)) = event::read() {
                 if key.code == KeyCode::Char('q') {
                     break;
                 }
@@ -22,4 +23,43 @@ fn main() -> std::io::Result<()> {
     execute!(stdout(), cursor::Show)?;
     terminal::disable_raw_mode()?;
     Ok(())
+}
+
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+struct Coordinates {
+    x: i32,
+    y: i32,
+}
+
+impl Coordinates {
+    fn new() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
+#[derive(Debug)]
+struct Snake {
+    body: Vec<Coordinates>,
+    direction: Direction,
+}
+
+impl Snake {
+    fn new() -> Self {
+        Self {
+            body: vec![Coordinates::new()],
+            direction: Direction::Up,
+        }
+    }
+
+    fn get_head_position(&self) -> &Coordinates {
+        &self.body[0]
+    }
 }
